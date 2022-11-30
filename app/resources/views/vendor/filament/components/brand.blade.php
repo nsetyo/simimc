6cfd411c36fdfaa7bac8e0b5eab5eea756b4514a
Setyo <setyo@pm.me>
2022-11-30 12:22:05 +0700

    Custom template

--- app/resources/views/vendor/filament/components/brand.blade.php
+++ app/resources/views/vendor/filament/components/brand.blade.php
@@ -1,4 +1,4 @@
 <div class="flex items-center">
     <img src="{{ asset('/logo.png') }}" alt="Logo" class="h-10">
-    <h3 class="font-bold text-2xl ml-2">SIM IMC</h3>
+    <h3 class="font-bold text-2xl ml-2 text-white">SIM IMC</h3>
 </div>
